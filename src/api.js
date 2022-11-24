b42662e593256b6b21783a9e3da5d7f4a1ca419f
Jasmail Singh Duck <jasmailduck@icloud.com>
2022-11-23 21:40:31 -0700

    Update api url to vm

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // an instance of the Axios driver. Used everywhere for API calls
 const instance = axios.create({
-  baseURL: "http://localhost:3003/api",
+  baseURL: "http://35.209.74.28:3003/api",
   headers: {
     "Content-Type": "application/json",
   }
